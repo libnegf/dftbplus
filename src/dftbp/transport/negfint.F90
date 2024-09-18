ff302deb73ec99406396b2e869e2fbe9f2a8f80e
Christoph Conrads <c.conrads@fz-juelich.de>
2024-09-18 19:29:50 +0200

    Transport: print timing information in CSV format

--- src/dftbp/transport/negfint.F90
+++ src/dftbp/transport/negfint.F90
@@ -1830,20 +1830,24 @@ contains
           write(timing_filename, "(A,I1,A)") "libnegf-timing-rank-", mpi_rank, ".txt"
           if(iKS == 1) then
             open(newunit=io, file=timing_filename, action="write")
+            write(io, '(A)') "mpi-rank,iKS,iK,iS,num-e-points,&
+                             &compute_current_wc,compute_current_cpu,&
+                             &contact_self_energies_wc,contact_self_energies_cpu,&
+                             &transmissions_and_dos_wc,transmissions_and_dos_cpu"
           else
             open(newunit=io, file=timing_filename, action="write", position="append")
           end if
           write(io, "(I3,A,I2,A,I2,A,I2,A,I3,A,I9,A,I10,A,I9,A,I10,A,I9,A,I10)") &
-            mpi_rank, " ", &
-            iKS, " ", &
-            iK, " ", &
-            iS, " ", &
-            num_e_points, " ", &
-            this%negf%timers%compute_current_wc / 1000 / 1000, " ", &
-            this%negf%timers%compute_current_cpu / 1000 / 1000, " ", &
-            this%negf%timers%contact_self_energies_wc / 1000 / 1000, " ", &
-            this%negf%timers%contact_self_energies_cpu / 1000 / 1000, " ", &
-            this%negf%timers%transmissions_and_dos_wc / 1000 / 1000, " ", &
+            mpi_rank, ",", &
+            iKS, ",", &
+            iK, ",", &
+            iS, ",", &
+            num_e_points, ",", &
+            this%negf%timers%compute_current_wc / 1000 / 1000, ",", &
+            this%negf%timers%compute_current_cpu / 1000 / 1000, ",", &
+            this%negf%timers%contact_self_energies_wc / 1000 / 1000, ",", &
+            this%negf%timers%contact_self_energies_cpu / 1000 / 1000, ",", &
+            this%negf%timers%transmissions_and_dos_wc / 1000 / 1000, ",", &
             this%negf%timers%transmissions_and_dos_cpu / 1000 / 1000
           close(io)
         end block
